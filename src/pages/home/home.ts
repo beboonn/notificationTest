26034d20c344298bb22effe051b30bbe31cabd3b
Punjaporn Kaewwisetkul <beboonn@Punjaporns-MacBook-Pro.local>
2017-11-07 21:47:56 +0700

    Local Noti is add

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController , AlertController , Platform } from 'ionic-angular';
+
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController , public alertCtl: AlertController , private plt: Platform , private localNotifications: LocalNotifications) {
+    this.plt.ready().then((rdy) => {
+      this.localNotifications.on('click', (notification, state) => {
+        let json = JSON.parse(notification.data);
+        let alert = this.alertCtl.create({
+          title: notification.title,
+          subTitle: json.mydata
+        });
+        alert.present();
+      });
+    });
+  }
 
+  scheduleNotification() {
+    this.localNotifications.schedule({
+      id: 1,
+      title: 'Hello',
+      text: 'this is Notification',
+      at: new Date(new Date().getTime() + 5 * 1000),
+      data: { mydata: 'My hidden message this is' }
+    });
   }
 
 }
